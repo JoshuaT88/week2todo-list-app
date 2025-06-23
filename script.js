dd3d3854b455114db53ca651268afbcdc5b25f05
JoshuaT88 <jatiller88@gmail.com>
2025-06-22 23:56:42 -0400

    enable task deletion on click & initial "mark as complete" logic

--- script.js
+++ script.js
@@ -8,13 +8,40 @@ const addBtn = document.getElementById("addBtn");
 addBtn.addEventListener("click", function addTask () {
     
 
-    const newLi = document.createElement("li");
-    const newContent = document.createTextNode(todoInput.value);
+    const listItem = document.createElement("li");
+    const newListItem = document.createTextNode(todoInput.value);
+    
 
-    newLi.appendChild(newContent);
+    listItem.appendChild(newListItem);
 
     const currentLi = document.getElementById("todoListItems");
-    currentLi.appendChild(newLi);
+    currentLi.appendChild(listItem);
+
+
+
+    const deleteButton = document.createElement("button");
+    const deleteListItem = document.createTextNode("🗑️");
+
+    listItem.appendChild(deleteButton);
+
+    deleteButton.appendChild(deleteListItem);
+    deleteButton.addEventListener("click", function deleteBtn() {
+        listItem.remove();
+    });
+
+
+
+    const completeButton = document.createElement("button");
+    const completeListItem = document.createTextNode("🔴");
+
+    listItem.appendChild(completeButton);
+
+    const completedListItem = document.getElementById("completedListItems");
+    completedListItem.appendChild(completeListItem);
+
+    completeButton.addEventListener("click", function completeBtn() {
+        listItem.classList.add();
+    });
 
 });
 
