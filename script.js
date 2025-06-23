7b3499123957fe7042e8a0cdd52cfa06f5abaedd
Joshua Tiller <jatiller88@gmail.com>
2025-06-23 16:43:24 -0400

    corrected task add, delete, and complete logic

--- script.js
+++ script.js
@@ -8,17 +8,19 @@ const addBtn = document.getElementById("addBtn");
 addBtn.addEventListener("click", function addTask () {
     
 
+    //add task to what to do section
     const listItem = document.createElement("li");
     const newListItem = document.createTextNode(todoInput.value);
     
 
     listItem.appendChild(newListItem);
-
+    todoInput.value = "";
+    
     const currentLi = document.getElementById("todoListItems");
     currentLi.appendChild(listItem);
 
 
-
+    //trash can button
     const deleteButton = document.createElement("button");
     const deleteListItem = document.createTextNode("🗑️");
 
@@ -30,18 +32,27 @@ addBtn.addEventListener("click", function addTask () {
     });
 
 
+    //add red button to click and add to Completed section
 
     const completeButton = document.createElement("button");
-    const completeListItem = document.createTextNode("🔴");
-
-    listItem.appendChild(completeButton);
+    completeButton.textContent = "🔴";
+    
 
     const completedListItem = document.getElementById("completedListItems");
-    completedListItem.appendChild(completeListItem);
+    listItem.appendChild(completeButton);
 
     completeButton.addEventListener("click", function completeBtn() {
-        listItem.classList.add();
-    });
+        listItem.classList.add("completed")
+   
 
+    //move completed item to "completed: " section
+    
+    completeButton.textContent = "🟢";
+
+    completedListItem.appendChild(listItem);
+    
+
+   
+ });
 });
 
